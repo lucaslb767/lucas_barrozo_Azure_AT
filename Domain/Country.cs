37eb62596dc102027c356f8537fc41f9ff6fc079
Lucas Lange Barrozo <lucas.barrozo@radixeng.com.br>
2020-09-23 11:41:31 -0300

    Creates State and Country Classes

--- Domain/Country.cs
+++ Domain/Country.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace Domain
 {
     public class Country
     {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Flag { get; set; }
+        [JsonIgnore]
+        public virtual IList<State> States { get; set; }
+    }
+    public class CountryResponse
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Flag { get; set; }
+        public virtual IList<State> States { get; set; }
     }
 }
--- Domain/State.cs
+++ Domain/State.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace Domain
 {
     public class State
     {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Flag { get; set; }
+        //[JsonIgnore]
+        public virtual Country Country { get; set; }
+    }
+
+    public class StateResponse
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Flag { get; set; }
+        public virtual Country Country { get; set; }
     }
 }
