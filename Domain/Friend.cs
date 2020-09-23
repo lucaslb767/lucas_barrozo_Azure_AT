8578a41c338c087cf70c8e2ae25e0d38e59e3a54
Lucas Lange Barrozo <lucas.barrozo@radixeng.com.br>
2020-09-23 11:14:40 -0300

    Add project files.

---
+++ Domain/Friend.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Domain
+{
+    public class Friend
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Telephone { get; set; }
+        public DateTime Bday { get; set; }
+
+        //[JsonIgnore]
+        public virtual Country Country { get; set; }
+        //[JsonIgnore]
+        public virtual State State { get; set; }
+        //[JsonIgnore]
+        public virtual IList<Brother> Friends { get; set; }
+    }
+
+    public class FriendResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Telephone { get; set; }
+        public DateTime Bday { get; set; }
+        public virtual Country Country { get; set; }
+        
+        public virtual State State { get; set; }
+       
+        public virtual IList<Brother> Friends { get; set; }
+    }
+}
