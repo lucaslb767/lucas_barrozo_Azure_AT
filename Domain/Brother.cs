96e37b3ba4abc8d8c7bfddfa81eff81315bb5db1
Lucas Lange Barrozo <lucas.barrozo@radixeng.com.br>
2020-09-23 11:21:04 -0300

    Creates Brother class

--- Domain/Brother.cs
+++ Domain/Brother.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace Domain
 {
     public class Brother
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Telephone { get; set; }
+        [JsonIgnore]
+        public Friend Friend { get; set; }
+    }
+
+    public class BrotherResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Telephone { get; set; }
+        public Friend Friend { get; set; }
     }
 }
