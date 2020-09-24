7bd002c1efb971a64e0ef021c47175f37d319e2a
Lucas Lange Barrozo <lucas.barrozo@radixeng.com.br>
2020-09-24 15:46:53 -0300

    Updates Home Controller

--- WebAzure/Controllers/HomeController.cs
+++ WebAzure/Controllers/HomeController.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RestSharp;
 using WebAzure.Models;
+using WebAzure.Models.HomeModel;
 
 namespace WebAzure.Controllers
 {
@@ -20,7 +23,21 @@ namespace WebAzure.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var client = new RestClient();
+
+            CreateHomeViewModel createHomeViewModel = new CreateHomeViewModel();
+
+            var requestFriend = new RestRequest("http://localhost:5003/api/friends", DataFormat.Json);
+            var responseFriend = client.Get<List<Friend>>(requestFriend);
+            createHomeViewModel.Friends = responseFriend.Data.Count;
+            var requestCountry = new RestRequest("http://localhost:5000/api/countries", DataFormat.Json);
+            var responseCountry = client.Get<List<Country>>(requestCountry);
+            createHomeViewModel.Countries = responseCountry.Data.Count;
+            var requestState = new RestRequest("http://localhost:5000/api/states", DataFormat.Json);
+            var responseState = client.Get<List<State>>(requestState);
+            createHomeViewModel.States = responseState.Data.Count;
+
+            return View(createHomeViewModel);
         }
 
         public IActionResult Privacy()
